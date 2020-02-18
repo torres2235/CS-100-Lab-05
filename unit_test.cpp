c8f2b4f9f90954827094d5c2974d2d6f2f2e541c
Torres2235 <Torres2235@gmail.com>
2020-02-17 20:33:51 -0800

    editting added headers

--- unit_test.cpp
+++ unit_test.cpp
@@ -1,12 +1,12 @@
 #include "gtest/gtest.h"
 
-#include "op_test.hpp"
-#include "rand_test.hpp"
-#include "mult_test.hpp"
-#include "div_test.hpp"
-#include "add_test.hpp"
-#include "sub_test.hpp"
-#include "pow_test.hpp"
+#include "lab-04/op_test.hpp"
+#include "lab-04/rand_test.hpp"
+#include "lab-04/mult_test.hpp"
+#include "lab-04/div_test.hpp"
+#include "lab-04/add_test.hpp"
+#include "lab-04/sub_test.hpp"
+#include "lab-04/pow_test.hpp"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
